17d41aeb327f4693eb9758d68f9ce8df8ab8696c
Mike Frazier <michael.s.frazier@gmail.com>
2016-04-20 19:35:26 -0500

    Add input field to index.html; begin implementation to handle to do submissions

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
   $('.toDoText').on('click', function() {
     $(this).toggleClass('completed');
   });
+  $('.submit').on('click', function(e) {
+    e.preventDefault();
+    handleSubmit();
+  })
+  
+  
+  function handleSubmit() {
+    console.log('This is submitting the input field');
+    var toDoObj = {
+      text: $('#toDoField').val()
+    }
+    console.log(toDoObj.text);
+  }
 });
